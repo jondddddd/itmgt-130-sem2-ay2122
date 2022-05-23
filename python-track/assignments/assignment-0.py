ec073eef5d5fa209d748750bd0dd7f93b9aa43ab
jondddddd <74131124+jondddddd@users.noreply.github.com>
2022-05-23 18:19:55 +0800

    Update assignment-0.py

--- python-track/assignments/assignment-0.py
+++ python-track/assignments/assignment-0.py
@@ -5,6 +5,9 @@ By completing this assignment, you will familiarize yourself with this
     course's submission mechanics.
 '''
 
+from audioop import avg
+
+
 def three_number_average(x, y, z):
     '''Item 1.
     Three Number Average. 3 points.
@@ -26,3 +29,9 @@ def three_number_average(x, y, z):
         the average of x, y, and z
     '''
     # Write your code below this line
+    three_number_average = (x+y+z)/3
+    print(three_number_average)
+    return three_number_average
+x=int(input())
+y=int(input())
+z=int(input())
